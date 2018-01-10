095576fbc3684f32479fff131bcd359337d45e37
tarea-ch2hw1-juego-de-cartas-21-JorgePucha created by GitHub Classroom <33530657+JorgePucha@users.noreply.github.com>
2018-01-10 12:53:31 -0500

    Update Principal.java

--- Examen1_JorgePucha/src/Clases/Principal.java
+++ Examen1_JorgePucha/src/Clases/Principal.java
@@ -34,23 +34,27 @@ public class Principal {
                     double taza = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el interes ejemplo 0.14 si es 14", "Cuenta Ahorro", JOptionPane.INFORMATION_MESSAGE));
                     CuentaAhorro cuentaA = new CuentaAhorro(nombre, tipocuenta, balance, taza);
                     ban.agregar(cuentaA);
+                    ban.grabar();
                     break;
                 case 2://MOSTRAR ELEMENTOS
                     String nombre1 = JOptionPane.showInputDialog(null, "Ingrese el nombre", "Cuenta Prestamo", JOptionPane.INFORMATION_MESSAGE);
-                    String tipocuenta1 = JOptionPane.showInputDialog(null, "Ingrese el nombre", "Cuenta Prestamo", JOptionPane.INFORMATION_MESSAGE);
+                    String tipocuenta1 = JOptionPane.showInputDialog(null, "Ingrese el tipo de cuenta (Natural o Empresa)", "Cuenta Prestamo", JOptionPane.INFORMATION_MESSAGE);
                     double balance1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el balance o monto", "Cuenta Prestamo", JOptionPane.INFORMATION_MESSAGE));
                     double taza1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el interes ejemplo 0.14 si es 14", "Cuenta Prestamo", JOptionPane.INFORMATION_MESSAGE));
                     CuentaPrestamo cuentaP = new CuentaPrestamo(nombre1, tipocuenta1, balance1, balance1);
                     ban.agregar(cuentaP);
+                    ban.grabar();
                     break;
                 case 3://INVERTIR Y MOSTRAR ELEMENTOS
                     String nombre2 = JOptionPane.showInputDialog(null, "Ingrese el nombre", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE);
-                    String tipocuenta2 = JOptionPane.showInputDialog(null, "Ingrese el nombre", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE);
-                    double balance2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el balance o monto", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE));
+                    String tipocuenta2 = JOptionPane.showInputDialog(null, "Ingrese el tipo de cuenta (Natural o Empresa)", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE);
+                        double balance2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el balance o monto", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE));
                     double taza2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el interes ejemplo 0.14 si es 14", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE));
                     CuentaHipoteca cuentaH = new CuentaHipoteca(nombre2, tipocuenta2, balance2, taza2);
                     int meses = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese los meses a pagar el prestamo", "Cuenta Hipoteca", JOptionPane.INFORMATION_MESSAGE));
+                    cuentaH.setMeses(meses);
                     ban.agregar(cuentaH);
+                    ban.grabar();
                     break;
                 case 4://SALIR
                     String menu2 = "Seleccione una de las siguientes opciones\n"
@@ -59,21 +63,23 @@ public class Principal {
                     int op2 = Integer.parseInt(JOptionPane.showInputDialog(null, menu2, "Cuenta de Ahorros", JOptionPane.INFORMATION_MESSAGE));
                     switch (op2) {
                         case 1://INGRESAR ELEMENTOS
-                            CuentaAhorro cuentaAH = (CuentaAhorro) ban.buscarCuenta();
+                            CuentaAhorro cuentaAH = (CuentaAhorro) ban.buscarCuentaAhorro();
                             double monto = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el monto", "Cuenta Ahorros", JOptionPane.INFORMATION_MESSAGE));
                             cuentaAH.depositar(monto);
                             System.out.println(cuentaAH.toString());
                             break;
                         case 2://INGRESAR ELEMENTOS
-                            CuentaAhorro cuentaAH2 = (CuentaAhorro) ban.buscarCuenta();
+                            CuentaAhorro cuentaAH2 = (CuentaAhorro) ban.buscarCuentaAhorro();
                             double monto2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el monto", "Cuenta Ahorros", JOptionPane.INFORMATION_MESSAGE));
                             cuentaAH2.depositar(monto2);
                             System.out.println(cuentaAH2.toString());
-                            break;
+                            break;    
                     }
+                    ban.grabar();
                 case 5://SALIR
-                    CuentaHipoteca cuentaH2 = (CuentaHipoteca) ban.buscarCuenta();
+                    CuentaHipoteca cuentaH2 = (CuentaHipoteca) ban.buscarCuentaHipoteca();
                     cuentaH2.depositarTazaInteres();
+                    ban.grabar();
                     break;
                 case 6://SALIR
                     break;
